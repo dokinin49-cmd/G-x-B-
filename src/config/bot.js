6bda90adf5396b002c204a35574eec7db1283459
dokinin49-cmd <dokinin49@gmail.com>
2026-07-04 21:06:02 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "By Antiguo Savage ❤️",
         // Activity type number (0 = Playing).
         type: 0,
       },
